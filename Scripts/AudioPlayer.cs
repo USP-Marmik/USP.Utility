911c1b0a3a7706382ab61cfc340e91e06d070a16
Marmik Emp37 <220137314+USP-Marmik@users.noreply.github.com>
2025-11-27 18:58:58 +0530

    Add AudioPlayer utility for audio playback
    Introduces the AudioPlayer MonoBehaviour to handle audio clip playback in Unity. Provides methods to play clips immediately or stop current playback before playing a new clip. Includes required AudioSource component and serialization for audio clips.

---
+++ Scripts/AudioPlayer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace USP.Utility
+{
+      [RequireComponent(typeof(AudioSource))]
+      public class AudioPlayer : MonoBehaviour
+      {
+            private AudioSource source;
+
+            [SerializeField] private AudioClip[] audioClips;
+
+
+            private void Awake()
+            {
+                  source = GetComponent<AudioSource>();
+            }
+
+            public void PlayImmediate(int index) => source.PlayOneShot(audioClips[index]);
+            public void Play(int index)
+            {
+                  if (source.isPlaying) source.Stop();
+                  PlayImmediate(index);
+            }
+      }
+}
