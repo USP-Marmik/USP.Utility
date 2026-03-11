863ea9184f80a3e15d75bc516a38dc0b397c6e07
Marmik Emp37 <marmikusp@gmail.com>
2026-03-11 12:26:35 +0530

    Update Hint.cs

--- Scripts/Hint.cs
+++ Scripts/Hint.cs
@@ -1,55 +1,54 @@
-using DG.Tweening;
-
 using UnityEngine;
 using UnityEngine.Events;
+using DG.Tweening;
 
 namespace USP.Utility
 {
-      public class Hint : MonoBehaviour
-      {
-            [Header("• T W E E N   S E T T I N G S")]
-            public float visibilityDuration = 0.2F;
-            public Ease visibilityEase = Ease.OutBack;
+	public class Hint : MonoBehaviour
+	{
+		[Header("• T W E E N   S E T T I N G S")]
+		public float visibilityDuration = 0.2F;
+		public Ease visibilityEase = Ease.OutBack;
 
-            private Tween visibilityTween, delayedCall;
+		private Tween visibilityTween, delayedCall;
 
-            public UnityEvent OnShow, OnHide;
+		public UnityEvent OnShow, OnHide;
 
 
-            private void Start()
-            {
-                  visibilityTween = transform.DOScale(Vector2.one, visibilityDuration).From(Vector2.zero)
-                        .SetEase(visibilityEase)
-                        .SetAutoKill(false)
-                        .OnKill(() => visibilityTween = null)
-                        .SetRecyclable(false)
-                        .Pause();
-            }
-            private void OnDestroy()
-            {
-                  visibilityTween?.Kill();
-            }
+		private void OnEnable()
+		{
+			visibilityTween = transform.DOScale(Vector2.one, visibilityDuration)
+				.From(Vector2.zero)
+				.SetEase(visibilityEase)
+				.SetAutoKill(false)
+				.OnKill(() => visibilityTween = null)
+				.Pause();
+		}
+		private void OnDisable()
+		{
+			visibilityTween?.Kill();
+		}
 
-            public void Show()
-            {
-                  delayedCall?.Kill();
+		public void Show()
+		{
+			delayedCall?.Kill();
 
-                  visibilityTween.PlayForward();
-                  OnShow.Invoke();
-            }
-            public void Show(float delay)
-            {
-                  visibilityTween.Restart(true, delay);
+			visibilityTween.PlayForward();
+			OnShow.Invoke();
+		}
+		public void Show(float delay)
+		{
+			visibilityTween.Restart(true, delay);
 
-                  delayedCall?.Kill();
-                  delayedCall = DOVirtual.DelayedCall(delay, OnShow.Invoke).OnKill(() => delayedCall = null);
-            }
-            public void Hide()
-            {
-                  delayedCall?.Kill();
+			delayedCall?.Kill();
+			delayedCall = DOVirtual.DelayedCall(delay, OnShow.Invoke).OnKill(() => delayedCall = null);
+		}
+		public void Hide()
+		{
+			delayedCall?.Kill();
 
-                  visibilityTween.SmoothRewind();
-                  OnHide.Invoke();
-            }
-      }
+			visibilityTween.SmoothRewind();
+			OnHide.Invoke();
+		}
+	}
 }
