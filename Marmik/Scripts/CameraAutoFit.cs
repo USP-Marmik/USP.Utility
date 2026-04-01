91f0f325e9c59032de46202683a9e69b660d2a55
Marmik Emp37 <220137314+USP-Marmik@users.noreply.github.com>
2026-04-01 21:48:22 +0530

    updated punch effect
    Signed-off-by: Marmik Emp37 <220137314+USP-Marmik@users.noreply.github.com>

--- Marmik/Scripts/CameraAutoFit.cs
+++ Marmik/Scripts/CameraAutoFit.cs
@@ -2,54 +2,54 @@ using UnityEngine;
 
 namespace USP.Utility
 {
-      [RequireComponent(typeof(Camera))]
-      public class CameraAutoFit : MonoBehaviour
-      {
-            public enum FitMode { Horizontal, Vertical }
+	[RequireComponent(typeof(Camera))]
+	public class CameraAutoFit : MonoBehaviour
+	{
+		public enum FitMode { Horizontal, Vertical }
 
-            public Camera Camera;
-            public SpriteRenderer Background;
-            public Vector2 Padding;
+		public Camera Camera;
+		public SpriteRenderer Background;
+		public Vector2 Padding;
 
-            public FitMode Mode;
-            public float MaxOrthographicSize = 5.4F;
+		public FitMode Mode;
+		public float MaxOrthographicSize = 5.4F;
 
-            [SerializeField] private bool autoApplyOnStart = true;
+		[SerializeField] private bool autoApplyOnStart = true;
 
-            public static bool IsWideAspect
-            {
-                  get
-                  {
-                        float aspect = (float) Screen.width / Screen.height;
-                        return aspect <= 1.29F || aspect >= 1.36F;
-                  }
-            }
+		public static bool IsWideAspect
+		{
+			get
+			{
+				float aspect = (float) Screen.width / Screen.height;
+				return aspect <= 1.29F || aspect >= 1.36F;
+			}
+		}
 
 
-            private void Reset()
-            {
-                  Camera = GetComponent<Camera>();
-            }
-            public void Start()
-            {
-                  if (autoApplyOnStart) Apply(Mode);
-            }
+		private void Reset()
+		{
+			Camera = GetComponent<Camera>();
+		}
+		public void Start()
+		{
+			if (autoApplyOnStart) Apply(Mode);
+		}
 
-            public void Apply(FitMode mode)
-            {
-                  float aspect = (float) Screen.width / Screen.height;
+		public void Apply(FitMode mode)
+		{
+			float aspect = (float) Screen.width / Screen.height;
 
-                  Bounds bounds = Background.bounds;
-                  bounds.Expand(new Vector3(Padding.x * 2F, Padding.y * 2F));
+			Bounds bounds = Background.bounds;
+			bounds.Expand(new Vector3(Padding.x * 2F, Padding.y * 2F));
 
-                  float target = Mathf.Min(mode switch { FitMode.Horizontal => bounds.extents.x / aspect, FitMode.Vertical => bounds.extents.y, _ => Camera.orthographicSize }, MaxOrthographicSize);
+			float target = Mathf.Min(mode switch { FitMode.Horizontal => bounds.extents.x / aspect, FitMode.Vertical => bounds.extents.y, _ => Camera.orthographicSize }, MaxOrthographicSize);
 
-                  Camera.orthographicSize = target;
+			Camera.orthographicSize = target;
 
-                  Vector3 position = Camera.transform.position;
-                  position.x = bounds.center.x;
-                  position.y = bounds.center.y;
-                  Camera.transform.position = position;
-            }
-      }
+			Vector3 position = Camera.transform.position;
+			position.x = bounds.center.x;
+			position.y = bounds.center.y;
+			Camera.transform.position = position;
+		}
+	}
 }
--- Marmik/Scripts/PunchEffect.cs
+++ Marmik/Scripts/PunchEffect.cs
@@ -4,22 +4,30 @@ using DG.Tweening;
 
 namespace USP.Utility
 {
-      public class PunchEffect : MonoBehaviour, IPointerDownHandler
-      {
-            public float Offset = 0.1F, Duration = 0.25F, Elasticity = 0.8F;
-            public int Vibration = 1;
+	public class PunchEffect : MonoBehaviour, IPointerDownHandler
+	{
+		public float Offset = 0.1F, Duration = 0.25F, Elasticity = 0.8F;
+		public int Vibration = 1;
 
-            private Tween pressTween;
+		private Tween pressTween;
 
 
-            public void OnPointerDown(PointerEventData _)
-            {
-                  pressTween ??= transform
-                        .DOPunchScale(Vector3.one * Offset, Duration, Vibration, Elasticity)
-                        .SetAutoKill(false)
-                        .Pause();
+		private void OnEnable()
+		{
+			pressTween = transform
+			   .DOPunchScale(Vector3.one * Offset, Duration, Vibration, Elasticity)
+			   .SetAutoKill(false)
+			   .OnKill(() => pressTween = null)
+			   .Pause();
+		}
+		private void OnDisable()
+		{
+			pressTween?.Kill();
+		}
 
-                  pressTween.Restart();
-            }
-      }
+		public void OnPointerDown(PointerEventData _)
+		{
+			pressTween.Restart();
+		}
+	}
 }
