60882058088896c8a3f41467213f72b7ab544add
Marmik Emp37 <220137314+USP-Marmik@users.noreply.github.com>
2026-04-02 15:36:04 +0530

    Update Log.cs

--- Marmik/Scripts/Log.cs
+++ Marmik/Scripts/Log.cs
@@ -5,14 +5,14 @@ using UObject = UnityEngine.Object;
 
 namespace USP.Utility
 {
-      public static class Log
-      {
-            public static bool Enabled { get; set; } = true;
+	public static class Log
+	{
+		public static bool Enabled { get; set; } = true;
 
 
-            private const string UNITY_EDITOR = "UNITY_EDITOR";
-            [Conditional(UNITY_EDITOR)] internal static void Info(object message, UObject context = null) { if (Enabled) Log(message, context); }
-            [Conditional(UNITY_EDITOR)] internal static void Warning(object message, UObject context = null) { if (Enabled) LogWarning(message, context); }
-            [Conditional(UNITY_EDITOR)] internal static void Error(object message, UObject context = null) { if (Enabled) LogError(message, context); }
-      }
+		private const string UNITY_EDITOR = nameof(UNITY_EDITOR);
+		[Conditional(UNITY_EDITOR)] internal static void Info(object message, UObject context = null) { if (Enabled) Log(message, context); }
+		[Conditional(UNITY_EDITOR)] internal static void Warning(object message, UObject context = null) { if (Enabled) LogWarning(message, context); }
+		[Conditional(UNITY_EDITOR)] internal static void Error(object message, UObject context = null) { if (Enabled) LogError(message, context); }
+	}
 }
