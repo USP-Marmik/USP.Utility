91490aa3d07fdc02eddb6b7b17780fe49f5147ac
Marmik Emp37 <marmikusp@gmail.com>
2026-08-13 10:43:06 +0530

    Update CameraAutoFit.cs

--- Marmik/Scripts/CameraAutoFit.cs
+++ Marmik/Scripts/CameraAutoFit.cs
@@ -7,22 +7,22 @@ namespace USP.Utility
 	{
 		public enum FitMode { Horizontal, Vertical }
 
-		public Camera Camera;
+		public new Camera camera;
 
-		[Space(2F)]
-		public float MaxOrthographicSize = 5.4F;
 
 		[Header("C O N F I G U R A T I O N")]
-		[SerializeField] private SpriteRenderer background;
-		[SerializeField] private Vector2 backgroundPadding;
-		[SerializeField] private FitMode mode;
+		public SpriteRenderer background;
+		public Vector2 backgroundPadding;
+		public FitMode mode;
 
+		[Space(2F)]
+		public float maxOrthographicSize = 5.4F;
 		[SerializeField] private bool autoApplyOnStart = true;
 
 
 		private void Reset()
 		{
-			Camera = GetComponent<Camera>();
+			camera = GetComponent<Camera>();
 		}
 		public void Start()
 		{
@@ -30,28 +30,24 @@ namespace USP.Utility
 		}
 
 		public void Apply() => Apply(mode, background, backgroundPadding);
-		public void Apply(FitMode mode) => Apply(mode, background, backgroundPadding);
 		public void Apply(FitMode mode, SpriteRenderer background, Vector2 padding = default)
 		{
-			if (background == null)
-			{
-				throw new System.ArgumentNullException(nameof(background));
-			}
+			if (background == null) throw new System.ArgumentNullException(nameof(background));
 
 			Bounds bounds = background.bounds;
 			bounds.Expand(new Vector3(padding.x * 2F, padding.y * 2F));
 
 			float aspect = (float) Screen.width / Screen.height;
 
-			float orthographicSize = mode switch { FitMode.Horizontal => bounds.extents.x / aspect, FitMode.Vertical => bounds.extents.y, _ => Camera.orthographicSize };
-			float target = Mathf.Min(orthographicSize, MaxOrthographicSize);
+			float orthographicSize = mode switch { FitMode.Horizontal => bounds.extents.x / aspect, FitMode.Vertical => bounds.extents.y, _ => camera.orthographicSize };
+			float target = Mathf.Min(orthographicSize, maxOrthographicSize);
 
-			Camera.orthographicSize = target;
+			camera.orthographicSize = target;
 
-			Vector3 position = Camera.transform.position;
+			Vector3 position = camera.transform.position;
 			position.x = bounds.center.x;
 			position.y = bounds.center.y;
-			Camera.transform.position = position;
+			camera.transform.position = position;
 		}
 	}
 }
